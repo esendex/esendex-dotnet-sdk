72baf277561cb431e73185d2d20f8d86d5adcd35
David Wood and Jack Thorley <development+codesleuth+jackthorley@esendex.com>
2016-05-06 17:06:17 +0100

    Add strong naming

--- source/Properties/AssemblyInfo.cs
+++ source/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -38,5 +37,3 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("2.2.0")]
 [assembly: AssemblyFileVersion("2.2.0")]
-[assembly: InternalsVisibleTo("com.esendex.sdk.test")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
---
+++ source/Properties/BuildInfo.cs
@@ -0,0 +1,1 @@
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo(@"com.esendex.sdk.test")]
