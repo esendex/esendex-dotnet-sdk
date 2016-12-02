212b5c5bc15375b6cbc057a6ad6ff3c33079a5a2
George Is a Butt Frost and James Turner <development+BnSmth+jamesturner@esendex.com>
2016-12-02 15:33:37 +0000

    SVY-271: Finish standard report endpoint + tests

--- source/surveys/SurveyReportService.cs
+++ source/surveys/SurveyReportService.cs
@@ -1,8 +1,8 @@
 using System;
-using System.Collections.Generic;
 using System.Net;
 using com.esendex.sdk.extensions;
 using com.esendex.sdk.http;
+using com.esendex.sdk.surveys.models;
 
 namespace com.esendex.sdk.surveys
 {
@@ -35,19 +35,17 @@ namespace com.esendex.sdk.surveys
         /// <param name="password">A string containing your password.</param>
         public SurveyReportService(string username, string password) : this(SURVEYS_BASE_URL, new EsendexCredentials(username, password)) {}
 
-
-
         /// <summary>
         /// Get a standard report for a specified date range and date range type.
         /// </summary>
         /// <param name="surveyId">A System.Guid that contains the Id of an active survey.</param>
-        /// <param name="type">An enum representing what to filter report rows on.</param>
         /// <param name="startDate">A DateTime representing the lower bound for the report date range.</param>
         /// <param name="endDate">A DateTime representing the upper bound for the report date range.</param>
-        /// <returns></returns>
-        public StandardReportResult GetStandardReport(Guid surveyId, DateRangeType type, DateTime startDate, DateTime endDate)
+        /// <param name="type">An enum representing what to filter report rows on. Defaults to 'QuestionSent'</param>
+        /// <returns>A StandardReportResult object containing any errors and the actual report rows</returns>
+        public StandardReportResult GetStandardReport(Guid surveyId, DateTime? startDate, DateTime? endDate, DateRangeType type = DateRangeType.QuestionSent)
         {
-            var request = Request.Create("GET", GetStandardReportUri(surveyId, type, startDate, endDate))
+            var request = Request.Create("GET", GetStandardReportUri(surveyId, startDate, endDate, type))
                                  .WithHeader("Authorization", "Basic " + _credentials.EncodedValue())
                                  .WithAcceptHeader(Constants.JSON_MEDIA_TYPE)
                                  .If(_credentials.UseProxy, r => r.WithProxy(_credentials.WebProxy));
@@ -67,7 +65,7 @@ namespace com.esendex.sdk.surveys
             return response.DeserialiseJson<StandardReportResult>();
         }
 
-        private Uri GetStandardReportUri(Guid surveyId, DateRangeType type, DateTime startDate, DateTime endDate)
+        private Uri GetStandardReportUri(Guid surveyId, DateTime? startDate, DateTime? endDate, DateRangeType? type)
         {
             var requestUrl = _baseUrl + string.Format("v1.0/surveys/{0}/report/standard", surveyId);
 
@@ -75,104 +73,30 @@ namespace com.esendex.sdk.surveys
 
             if (type == DateRangeType.QuestionSent)
             {
-                builder.WithParameter("questionSentAfter", startDate.ToString("O"));
-                builder.WithParameter("questionSentBefore", endDate.ToString("O"));
+                if (startDate != null)
+                {
+                    builder.WithParameter("questionSentAfter", startDate.Value.ToString("O"));
+                }
+
+                if (endDate != null)
+                {
+                    builder.WithParameter("questionSentBefore", endDate.Value.ToString("O"));
+                }
             }
             else
             {
-                builder.WithParameter("answerReceivedAfter", startDate.ToString("O"));
-                builder.WithParameter("answerReceivedBefore", endDate.ToString("O"));
+                if (startDate != null)
+                {
+                    builder.WithParameter("answerReceivedAfter", startDate.Value.ToString("O"));
+                }
+
+                if (endDate != null)
+                {
+                    builder.WithParameter("answerReceivedBefore", endDate.Value.ToString("O"));
+                }
             }
 
             return builder.Build();
         }
-
-    }
-
-    /// <summary>
-    /// A class containing an array of SurveyReportErrors.
-    /// </summary>
-    public class StandardReportResult
-    {
-        /// <summary>
-        /// An array of StandardReportErrors.
-        /// </summary>
-        public SurveyReportError[] Errors { get; set; }
-        /// <summary>
-        /// An array of StandardReportRows.
-        /// </summary>
-        public StandardReportRow[] Rows { get; set; }
-    }
-
-    /// <summary>
-    /// Represents a question sent to a recipient and the answer received.
-    /// </summary>
-    public class StandardReportRow
-    {
-        /// <summary>
-        /// A string containing the phone number of a recipient.
-        /// </summary>
-        public string Recipient { get; set; }
-
-        /// <summary>
-        /// A string containing the delivery status of the question sent.
-        /// </summary>
-        public string DeliveryStatus { get; set; }
-
-        /// <summary>
-        /// A string containing the label of the question.
-        /// </summary>
-        public string QuestionLabel { get; set; }
-
-        /// <summary>
-        /// A DateTime representing when the question was sent.
-        /// </summary>
-        public DateTime QuestionDateTime { get; set; }
-
-        /// <summary>
-        /// A string containing the label of the answer received.
-        /// </summary>
-        public string AnswerLabel { get; set; }
-
-        /// <summary>
-        /// A nullable DateTime representing when the answer was received.
-        /// </summary>
-        public DateTime? AnswerDateTime { get; set; }
-
-        /// <summary>
-        /// A string containing the recipient's response to the question.
-        /// </summary>
-        public string AnswerText { get; set; }
-
-        /// <summary>
-        /// A dictionary containing both the template fields and metadata saved when adding the recipient to the survey.
-        /// </summary>
-        public Dictionary<string, string> RecipientData { get; set; }
-
-    }
-
-    /// <summary>
-    /// An error containing a code and description.
-    /// </summary>
-    public class SurveyReportError
-    {
-        public string Code { get; set; }
-        public string Description { get; set; }
-    }
-
-    /// <summary>
-    /// An enum representing what to filter report rows on.
-    /// </summary>
-    public enum DateRangeType
-    {
-        /// <summary>
-        /// Filter report rows by the date questions were sent
-        /// </summary>
-        QuestionSent,
-
-        /// <summary>
-        /// Filter report rows by the date answers were received
-        /// </summary>
-        AnswerReceived
     }
 }
---
+++ test/surveys/report/StandardReportWithBadRequestTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using com.esendex.sdk.surveys;
+using com.esendex.sdk.surveys.models;
+using com.esendex.sdk.test.mockapi;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace com.esendex.sdk.test.surveys.report
+{
+    [TestFixture]
+    public class GivenABadRequestError
+    {
+        private Guid _surveyId;
+        private DateTime _startDate;
+        private DateTime _endDate;
+        private StandardReportResult _result;
+        private StandardReportResult _apiResponse;
+
+        [TestFixtureSetUp]
+        public void WhenGettingTheStandardReport()
+        {
+            const string username = "user@example.com";
+            const string password = "heythiscantbeguessed";
+            _surveyId = Guid.NewGuid();
+            _startDate = new DateTime(2016, 10, 01);
+            _endDate = new DateTime(2016, 10, 25);
+
+            _apiResponse = new StandardReportResult
+            {
+                Errors = new[]
+                {
+                    new SurveyReportError {Code = "date_in_future", Description = "Cannot download report for dates in future"}
+                }
+            };
+
+            MockApi.SetEndpoint(new MockEndpoint(400, JsonConvert.SerializeObject(_apiResponse), "application/json"));
+
+            var surveyReportService = new SurveyReportService(MockApi.Url, new EsendexCredentials(username, password));
+            _result = surveyReportService.GetStandardReport(_surveyId, _startDate, _endDate);
+        }
+
+        [Test]
+        public void ThenTheResultContainsTheErrors()
+        {
+            Assert.That(_result.Errors[0].Code, Is.EqualTo(_apiResponse.Errors[0].Code));
+            Assert.That(_result.Errors[0].Description, Is.EqualTo(_apiResponse.Errors[0].Description));
+        }
+    }
+}
